f3170ccbac230dd4049889ac69afd6c92ac0af1d
bexruz-cyber <mayoq2024@gmail.com>
2025-11-11 15:39:35 +0300

    done

--- public/js/app.js
+++ public/js/app.js
@@ -22,3 +22,24 @@ document.addEventListener('keydown', e => {
     toggleSidebar()
   }
 })
+
+function toggleAccordion (btn) {
+  const allAccordions = document.querySelectorAll('.accordion-btn')
+  const content = btn.nextElementSibling
+  const isOpen = btn.classList.contains('open')
+
+  allAccordions.forEach(b => {
+    if (b !== btn) {
+      b.classList.remove('open')
+      b.nextElementSibling.style.maxHeight = '0px'
+    }
+  })
+
+  if (!isOpen) {
+    btn.classList.add('open')
+    content.style.maxHeight = content.scrollHeight + 'px'
+  } else {
+    btn.classList.remove('open')
+    content.style.maxHeight = '0px'
+  }
+}
